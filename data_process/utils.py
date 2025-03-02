a097cc8389e3bb6e7cff7406a8e8ce7cd5f9dd80
San Lamamba P. <contact@sanlamamba.com>
2025-03-02 16:40:02 +0000

    feat ( process ) added more CRS types to utils

--- data_process/utils.py
+++ data_process/utils.py
@@ -1,6 +1,14 @@
 from shapely.geometry import Point, LineString
 from shapely.ops import transform
 from shapely.geometry import shape
+from enum import Enum
+import pyproj
+
+
+class CRS(Enum):
+    EPSG_4326 = "4326"  # WGS 84
+    EPSG_2154 = "2154"  # RGF93 / Lambert-93
+    EPSG_3857 = "3857"  # Pseudo-Mercators
 
 
 def get_endpoints(geom):
@@ -34,3 +42,33 @@ def get_coordinates(geom_dict, transformer):
     geom_4326 = transform(transformer.transform, geom_obj)
     coords_4326 = geom_4326.__geo_interface__["coordinates"]
     return coords_4326
+
+
+def convert_coordinates(geom_dict, from_crs: CRS = None, to_crs: CRS = None):
+    """
+    Convert geometry coordinates from one CRS to another.
+
+    :param geom_dict: A geometry dictionary (from __geo_interface__)
+    :param from_crs: The source CRS (as a CRS Enum, optional)
+    :param to_crs: The target CRS (as a CRS Enum, optional)
+    :return: Converted coordinates in the target CRS or all CRSs if no specific CRS is provided.
+    """
+    geom_obj = shape(geom_dict)
+
+    if from_crs and to_crs:
+        transformer = pyproj.Transformer.from_crs(
+            f"EPSG:{from_crs.value}", f"EPSG:{to_crs.value}", always_xy=True
+        )
+        transformed_geom = transform(transformer.transform, geom_obj)
+        return transformed_geom.__geo_interface__["coordinates"]
+
+    all_results = {}
+    for crs in CRS:
+        transformer = pyproj.Transformer.from_crs(
+            f"EPSG:{CRS.EPSG_4326.value}", f"EPSG:{crs.value}", always_xy=True
+        )
+        all_results[crs.name] = transform(
+            transformer.transform, geom_obj
+        ).__geo_interface__["coordinates"]
+
+    return all_results
